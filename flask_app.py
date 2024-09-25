173fd20a711b0d5321369c599a1c71e6de03d4ea
IgnorantWisdomJVB <jaredatjared@gmail.com>
2024-09-25 18:18:34 -0500

    pushing alpha version

--- flask_app.py
+++ flask_app.py
@@ -8,5 +8,10 @@ app = Flask(__name__)
 def hello_world():
     return render_template('index.html')
 
+
+@app.route('/alpha')
+def alpha():
+    return render_template('alpha.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
